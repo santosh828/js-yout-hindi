be9063d71c35301b0b217ce75059f60ca8b2a23d
santoshkumarkamat-design <santoshkumarkamat@amityonline.com>
2026-07-31 09:07:20 +0000

    santosh

---
+++ .devcontainer/santosh/01_varibal.js
@@ -0,0 +1,9 @@
+const accounit=123487
+let accountemil="santos@gold.com"
+var accountpassword="santosh@123"
+acountcity="delhi"
+let accountstate="delhi"
+
+
+console.log(accounit);
+console.table([accounit,accountemil,accountpassword,acountcity,accountstate]);
