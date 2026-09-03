df38472763e00b5141fe2a89ee6bc83a039dae7d
santosh828 <santoshkumarkamat@amityonline.com>
2026-09-03 10:17:38 +0000

    hi

---
+++ java_besic/test.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+let name="santosh"
+console.log(name)
+let age=25
+console.let(age)
+let city ="delhi"
+console.let(city)
+let a=10
+let b=10
+let sum=(a+b)
+console.log(sum)
+
